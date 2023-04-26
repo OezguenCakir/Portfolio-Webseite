b33d35c35fd84e254f151b8c461628e254fe973e
Özgün Cakir <103118042+OezguenCakir@users.noreply.github.com>
2023-04-26 23:02:24 +0200

    :sparkles: eig nicht viel neues (#1)
    * new branch
    
    * viel formatting
    
    * neu

--- scripts/content.js
+++ scripts/content.js
@@ -1,39 +1,40 @@
-if(window.location.href.indexOf("#en") > -1) {
-    document.getElementById("langEN").style.backgroundColor = "#004f7b";
+if (window.location.href.indexOf("#en") > -1) {
+  document.getElementById("langEN").style.backgroundColor = "#004f7b";
 }
-if(window.location.href.indexOf("#de") > -1) {
-    document.getElementById("langDE").style.backgroundColor = "#004f7b";
+if (window.location.href.indexOf("#de") > -1) {
+  document.getElementById("langDE").style.backgroundColor = "#004f7b";
 }
 
 // Übersetzungen
 var language = {
-    en: {
-        welcome: "Hello, I'm Özgün.",
-        aboutMe: "Hi I'm cool. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        summary: "About Me",
-        jobs: "Work experience",
-        certification: "Certifications",
-        education: "Education",
-        contact: "Contact",
-        MailList: "Write to me at one of my email addresses:",
-        techSkills: "Technical skills",
-        selfDestruction: "Self destruction",
-        MailListInfo: "These addresses actually exist"
-    }
+  en: {
+    welcome: "Hello, I'm Özgün.",
+    aboutMe:
+      "Hi I'm cool. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    summary: "About Me",
+    jobs: "Work experience",
+    certification: "Certifications",
+    education: "Education",
+    contact: "Contact",
+    MailList: "Write to me at one of my email addresses:",
+    techSkills: "Technical skills",
+    selfDestruction: "Self destruction",
+    MailListInfo: "These addresses actually exist",
+  },
 };
 
 // define language via window hash
 if (window.location.hash) {
-    if (window.location.hash === "#en") {
-        welcome.textContent = language.en.welcome
-        aboutMe.textContent = language.en.aboutMe
-        summ.textContent = language.en.summary
-        jobs.textContent = language.en.jobs
-        certification.textContent = language.en.certification
-        education.textContent = language.en.education
-        contactMe.textContent = language.en.contact
-        MailList.textContent = language.en.MailList
-        techSkills.textContent = language.en.techSkills
-        selfDestruction.textContent = language.en.selfDestruction
-    }
+  if (window.location.hash === "#en") {
+    welcome.textContent = language.en.welcome;
+    aboutMe.textContent = language.en.aboutMe;
+    summ.textContent = language.en.summary;
+    jobs.textContent = language.en.jobs;
+    certification.textContent = language.en.certification;
+    education.textContent = language.en.education;
+    contactMe.textContent = language.en.contact;
+    MailList.textContent = language.en.MailList;
+    techSkills.textContent = language.en.techSkills;
+    selfDestruction.textContent = language.en.selfDestruction;
+  }
 }
