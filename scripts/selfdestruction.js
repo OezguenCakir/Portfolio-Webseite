a38c15c794f0e994f3a67d454a2c64f6d3b798a1
OezguenCakir <103118042+OezguenCakir@users.noreply.github.com>
2023-04-30 22:01:20 +0200

    :hammer: ein paar Änderungen

--- scripts/selfdestruction.js
+++ scripts/selfdestruction.js
@@ -1,24 +1,7 @@
 $(function () {
-  // run the currently selected effect
   function runEffect() {
-    // get effect type from
-    var selectedEffect = "scale";
-
-    // Most effect types need no options passed by default
-    var options = {};
-
-    // Run the effect
-    $("body").hide(selectedEffect, options, 1000, callback);
+    $("body").hide("scale", {}, 1000);
   }
-
-  // Callback function to bring a hidden box back
-  function callback() {
-    setTimeout(function () {
-      $("body").removeAttr("style").hide().fadeIn();
-    }, 10000000);
-  }
-
-  // Set effect from select menu value
   $("#selfDestruction").on("click", function () {
     runEffect();
   });
